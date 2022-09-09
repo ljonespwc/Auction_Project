9a94581947a6e04914e2a36ca63b7115436bbddb
Lance Jones <lancecj@gmail.com>
2022-09-09 09:24:55 -0700

    working chart.js and pandas

---
+++ auction/chart.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from flask import Flask, Blueprint, render_template
+import psycopg2
+from sqlalchemy import create_engine
+
+chart = Blueprint('chart', __name__)
+
+@chart.route('/chart')
+def draw_chart():
+    
+    DATABASE_URI = 'postgresql+psycopg2://postgres:Porsche911@localhost:5432/postgres'
+    engine = create_engine(DATABASE_URI)
+    dbConnection = engine.connect()
+    
+    df = pd.read_sql("""
+                        select extract(year from completion_date) AS auctionyear,
+                        percentile_cont(0.50) within group (order by price) as price
+                        from listings
+                        where model_name = 'Porsche 911 GT3' and status = 'Sold'
+                        group by model_name, auctionyear
+                        order by auctionyear ASC
+                    """, dbConnection)
+    
+    # dbConnection.close()
+
+    auctionyear = df['auctionyear'].astype(int).values.tolist() # x-axis
+    price = df['price'].values.astype(int).tolist() # y-axis
+
+    return render_template('chart.html', auctionyear=auctionyear, price=price)
