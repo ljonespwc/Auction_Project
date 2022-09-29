5aa29256755d1c6675483b68b39f294b5430743a
Lance Jones <lancecj@gmail.com>
2022-09-29 15:10:35 -0700

    model dropdown working

--- auction/views.py
+++ auction/views.py
@@ -42,24 +42,54 @@ def home():
 @views.route('/chart')
 def draw_chart():
     session = db_connect()
-    make = request.args.get('make')
     
-    df = pd.read_sql("""
-                        select extract(year from completion_date) AS auctionyear,
-                        count(*) as listingcount,
-                        percentile_cont(0.50) within group (order by price) as price
-                        from listings
-                        where make = '%s' and status = 'Sold' and extract(year from completion_date) <> 2014
-                        group by make, auctionyear
-                        order by auctionyear ASC
-                    """ % make, session.connection())
+    make = request.args.get('make')
+    df_dropdown = pd.read_sql("""
+                            SELECT DISTINCT model_name
+                            FROM models INNER JOIN makes ON models.make_id = makes.make_id
+                            WHERE makes.make = '%s'
+                            ORDER BY model_name ASC
+                            """ % make, session.connection())
+    dropdown_data = df_dropdown.reset_index()['model_name'].values.tolist()
+    
+    if not request.args.get('model'): # if make is selected from home page, show make data
+        
+        df = pd.read_sql("""
+                            select extract(year from completion_date) AS auctionyear,
+                            count(*) as listingcount,
+                            percentile_cont(0.50) within group (order by price) as price
+                            from listings
+                            where make = '%s' and status = 'Sold' and extract(year from completion_date) <> 2014
+                            group by make, auctionyear
+                            order by auctionyear ASC
+                        """ % make, session.connection())
 
-    auctionyear = df['auctionyear'].astype(int).values.tolist() # chart x-axis
-    price = df['price'].values.astype(int).tolist() # chart y-axis
-    supporting_data = df.reset_index()[['auctionyear', 'listingcount']].values.astype(int).tolist()
+        auctionyear = df['auctionyear'].astype(int).values.tolist() # chart x-axis
+        price = df['price'].values.astype(int).tolist() # chart y-axis
+        supporting_data = df.reset_index()[['auctionyear', 'listingcount']].values.astype(int).tolist()
+        session.close()
+        return render_template('chart.html', make=make, auctionyear=auctionyear, price=price,
+                               supporting_data=supporting_data, dropdown_data=dropdown_data)
+    
+    else: # if model is selected from dropdown on chart page, show model data
 
-    session.close()
-    return render_template('chart.html', make=make, auctionyear=auctionyear, price=price, supporting_data=supporting_data)
+        model = request.args.get('model')
+        df = pd.read_sql("""
+                            select extract(year from completion_date) AS auctionyear,
+                            count(*) as listingcount,
+                            percentile_cont(0.50) within group (order by price) as price
+                            from listings
+                            where model_name = '%s' and status = 'Sold' and extract(year from completion_date) <> 2014
+                            group by model_name, auctionyear
+                            order by auctionyear ASC
+                        """ % model, session.connection())
+
+        auctionyear = df['auctionyear'].astype(int).values.tolist() # chart x-axis
+        price = df['price'].values.astype(int).tolist() # chart y-axis
+        supporting_data = df.reset_index()[['auctionyear', 'listingcount']].values.astype(int).tolist()
+        session.close()
+        return render_template('chart.html', make=make, model=model, auctionyear=auctionyear, price=price,
+                               supporting_data=supporting_data, dropdown_data=dropdown_data)
 
 def db_connect():
     DATABASE_URI = os.getenv("DATABASE_URI")
